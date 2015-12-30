ba6515195bebf6c2bec5e24a85779266695a0782
Daniel D <daniel@klangfiguren.com>
2015-12-30 17:48:45 +0100

    Replaced all cout occurences with generic output function to support windows specific DebugOutput

--- src/main.cpp
+++ src/main.cpp
@@ -7,26 +7,46 @@
 //
 
 #include <iostream>
+#include <string>
+#include <Windows.h>
 #include "../include/HashMap.hpp"
 #include "Constants.hpp"
 
 using namespace std;
-/**
-int main(int argc, const char * argv[]) {
+
+void platformOutl( string s ) {
+	stringstream ss;
+	ss << s << endl;
+
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32)
+	OutputDebugString( ss.str().c_str() );
+#else
+	cout( ss.str() );
+#endif
+
+}
+
+int main( int argc, const char * argv[] ) {
+
 	HashMap<int, string> hmap;
-	hmap.put(1, "val1");
-	hmap.put(2, "val2");
-	hmap.put(3, "val3");
+	hmap.put( 1, "val1" );
+	hmap.put( 2, "val2" );
+	hmap.put( 3, "val3" );
 
 	string value;
-	hmap.get(2, value);
-	cout << value << endl;
-	bool res = hmap.get(3, value);
-	if (res)
-	    cout << value << endl;
-	hmap.remove(3);
-	res = hmap.get(3, value);
-	if (res)
-	    cout << value << endl;
+	hmap.get( 2, value );
+	platformOutl( value );
+	bool res = hmap.get( 3, value );
+	if ( res )
+		platformOutl( value );
+	hmap.remove( 3 );
+	res = hmap.get( 3, value );
+
+	for ( int i = 0; i < 120; i++ ) {
+		hmap.put( i, "asd" );
+	}
+
+	if ( res )
+		platformOutl( value );;
 }
-*/
+
