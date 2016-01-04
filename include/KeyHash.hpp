9fa9c83cac4242030f150a6af5a7e148b64e5e2d
pgunia <patrick.gunia@gmail.com>
2016-01-04 15:05:46 +0100

    Revert "Revert "Revert "Added shared_timed_mutex for RW-lock support Minor clean-ups Added RW Locks"""
    This reverts commit 3a75a43017b67cab32d58c55a2e01540e4b4a745.

--- include/KeyHash.hpp
+++ include/KeyHash.hpp
@@ -13,12 +13,12 @@
 // Default hash function class
 template <typename K>
 struct KeyHash {
-
+	/*
     unsigned long operator()(const K& key) const
     {
         return reinterpret_cast<unsigned long>(key) % constants::TABLE_SIZE;
     }
-
+    */
 };
 
 #endif /* KEYHASH_HPP_ */
